4f40816da64cf88c5bfcfabd526b7b10692f309e
cmarvin <chris.j.marvin@gmail.com>
2014-10-28 14:48:36 +0100

    Fixed kernel values to those in email.

--- core/Simulator.py
+++ core/Simulator.py
@@ -423,8 +423,8 @@ class Simulator(object):
         if not kernel:
             kernel = np.array([[0, 0, 1, 0, 0],
                                [0, 2, 2, 2, 0],
-                               [0, 2, 5, 2, 0],
+                               [1, 2, 5, 2, 1],
                                [0, 2, 2, 2, 0],
-                               [0, 0, 0, 0, 0]], dtype='int16')
+                               [0, 0, 1, 0, 0]], dtype='int16')
 
         self.outarr = scipy.ndimage.filters.convolve(self.outarr, kernel)
